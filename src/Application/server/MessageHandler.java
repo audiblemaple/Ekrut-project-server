030997522546167227b2b50cea10442f7f9369d2
audiblemaple <audiblemaple@gmail.com>
2022-12-24 22:46:49 +0200

    .

--- src/Application/server/MessageHandler.java
+++ src/Application/server/MessageHandler.java
@@ -124,9 +124,6 @@ public class MessageHandler {
                 break;
 
             case "REQUEST_MACHINE_MONTHLY_INVENTORY_REPORT":
-                mysqlcontroller.generateMonthlyInventoryReport("tel aviv", "TA01", "01", "2021");
-
-
                 ArrayList<String> monthYearMachine = (ArrayList<String>) message.getData();
                 InventoryReport report =  mysqlcontroller.getMonthlyInventoryReport(monthYearMachine);
                 if(report != null){
