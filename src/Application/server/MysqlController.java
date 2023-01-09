e9ebdb6c694f7282443fb4efd17aa4ac37157b6d
lior <audiblemaple@gmail.com>
2023-01-09 14:33:50 +0200

    disconnect logs everyone out

--- src/Application/server/MysqlController.java
+++ src/Application/server/MysqlController.java
@@ -1849,6 +1849,103 @@ public class MysqlController {
 	}
 
 
+	public void importMechanism(){
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+//import java.io.FileReader;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.Statement;
+//
+//import com.opencsv.CSVReader;
+//
+//		private static void readCsv() {
+//
+//			try (CSVReader reader = new CSVReader(new FileReader("upload.csv"), ',');
+//				 Connection connection = DBConnection.getConnection();) {
+//				String insertQuery = "Insert into txn_tbl (txn_id,txn_amount, card_number, terminal_id) values (null,?,?,?)";
+//				PreparedStatement pstmt = connection.prepareStatement(insertQuery);
+//				String[] rowData = null;
+//				int i = 0;
+//				while((rowData = reader.readNext()) != null) {
+//					for (String data : rowData) {
+//						pstmt.setString((i % 3) + 1, data);
+//
+//						if (++i % 3 == 0)
+//							pstmt.addBatch();// add batch
+//
+//						if (i % 30 == 0)// insert when the batch size is 10
+//							pstmt.executeBatch();
+//					}
+//				}
+//				System.out.println("Data Successfully Uploaded");
+//			}
+//			catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//
+//		}
+//
+//		private static void readCsvUsingLoad() {
+//			try (Connection connection = DBConnection.getConnection()) {
+//
+//				String loadQuery = "LOAD DATA LOCAL INFILE '" + "C:\\upload.csv" + "' INTO TABLE txn_tbl FIELDS TERMINATED BY ','" + " LINES TERMINATED BY '\n' (txn_amount, card_number, terminal_id) ";
+//				System.out.println(loadQuery);
+//				Statement stmt = connection.createStatement();
+//				stmt.execute(loadQuery);
+//			}
+//			catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public void generateReports(){
 		LocalDate date = LocalDate.now();
 		int year = date.getYear();
